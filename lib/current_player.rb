377bfe0f234924824dd3ce4a9a99a980fe1a396c
Daniel Cripps <dan-cripps@hotmail.co.uk>
2019-10-06 11:10:07 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,13 @@
-def turn_count  do
-
+def turn_count(board)
+  counter = 0
+  board.each do |i|
+    if i == "X" || i == "O"
+      counter += 1
+    end
+  end
+  return counter
 end
 
-def current_player do
-  
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
 end
